9ffda3731427f57f3d0084cf4e52fb0a955f9583
Ljavuras <ljavuras.py@gmail.com>
2026-01-23 01:19:22 +0800

    feat(evergarden-design-system): provides building block for a coherent look close to native Obsidian

---
+++ System/packages/evergarden-design-system/Evergarden design system.md
@@ -0,0 +1,76 @@
+---
+created: 2026-01-05T09:56:50.387+08:00
+---
+# Evergarden design system
+
+## Primitive
+
+```datacoretsx
+const { obsidian, Datacore } = await cJS();
+const { Notice } = obsidian;
+
+dc = Datacore.wrap(dc);
+dc.config.dev = true;
+
+const {
+    Pill,
+    InternalLink,
+    ExternalLink,
+    Button,
+    ClickableIcon,
+} = await dc.require("evergarden-design-system", "Primitive");
+
+const link = dc.currentFile()?.$link;
+
+function Group({ children, ...props }) {
+    return (
+        <div
+            style={{
+                display: "flex",
+                gap: "var(--size-4-2)",
+                "align-items": "baseline",
+                margin: "var(--size-4-2) 0",
+            }}
+        >
+            {children}
+        </div>
+    )
+}
+
+return (<>
+    <Group>
+        <InternalLink link={link}>internal link</InternalLink>
+        <ExternalLink href="obsidian.md">external link</ExternalLink>
+    </Group>
+    <Group>
+        <Pill>text pill</Pill>
+        <Pill link={link}>linked pill</Pill>
+    </Group>
+    <Group>
+        <Button onClick={() => new Notice("Regular button")}>Button</Button>
+        <Button onClick={() => new Notice("Call to action")} mod="cta">
+            CTA button
+        </Button>
+        <Button onClick={() => new Notice("Warning")} mod="warning">
+            Warning
+        </Button>
+        <Button onClick={() => new Notice("Destructive action")} mod="destructive">
+            Destructive
+        </Button>
+        <Button onClick={() => new Notice("Loading...")} mod="loading">
+            Loading
+        </Button>
+    </Group>
+    <Group>
+        <ClickableIcon
+            onClick={() => new Notice("Clickable icon")}
+            icon="info"
+        />
+        <ClickableIcon
+            onClick={() => new Notice("Active clickable icon")}
+            icon="activity"
+            isActive={true}
+        />
+    </Group>
+</>)
+```
---
+++ System/packages/evergarden-design-system/datacore/Primitive.tsx
@@ -0,0 +1,164 @@
+/**
+ * @author Ljavuras <ljavuras.py@gmail.com>
+ */
+
+const { app, obsidian } = await cJS();
+
+function InternalLink({ link, className, children, ...props }) {
+    return (
+        <a
+            aria-label={link.displayOrDefault()}
+            className={"internal-link " + className}
+            target="_blank"
+            rel="noopener"
+            data-tooltip-position="top"
+            data-href={link.obsidianLink()}
+            href={link.obsidianLink()}
+            {...props}
+        >
+            {children ?? link.displayOrDefault()}
+        </a>
+    )
+}
+
+/**
+ * Attach internal links' click (navigate to note) and mouseover (show hover
+ * preview) event handlers by itself. Use this component when the internal
+ * link isn't contained in `MarkdownView`, which attaches the handlers
+ * automatically for internal link anchor tags.
+ * 
+ * {@link https://forum.obsidian.md/t/internal-links-dont-work-in-custom-view/90169/3}
+ * {@link https://github.com/mnaoumov/obsidian-dev-utils/blob/6e744dd2b142b6bbf7d9a4f99f3635fa91a2a6b6/src/obsidian/Markdown.ts#L165}
+ */
+function SelfHandledInternalLink({
+    link,
+    sourcePath="",
+    ...props
+}) {
+    return (
+        <InternalLink
+            link={link}
+            onClick={event => {
+                const linktext = link.obsidianLink();
+                if (linktext) {
+                    // onInternalLinkClick
+                    app.workspace.openLinkText(
+                        linktext,
+                        sourcePath,
+                        obsidian.Keymap.isModEvent(event)
+                    );
+                }
+            }}
+            onMouseOver={event => {
+                const linktext = link.obsidianLink();
+                if (linktext) {
+                    // onInternalLinkMouseover
+                    app.workspace.trigger("hover-link", {
+                        event,
+                        source: "preview",
+                        hoverParent: { hoverPopover: null },
+                        targetEl: event.currentTarget,
+                        linktext: linktext,
+                        sourcePath: sourcePath,
+                    });
+                }
+            }}
+            {...props}
+        />
+    )
+}
+
+function ExternalLink({ href, className, children, ...props }) {
+    return (
+        <a
+            rel="noopener nofollow"
+            className={"external-link " + className}
+            href={href}
+            target="_blank"
+        >
+            {children ?? href}
+        </a>
+    )
+}
+
+function Pill({ className, children, link, ...props }) {
+    return (link
+        ? <InternalLink
+            className={className
+                ? "evergarden pill interactive " + className
+                : "evergarden pill interactive"
+            }
+            link={link}
+            {...props}
+        >
+            {children}
+        </InternalLink>
+        : <span
+            className={className
+                ? "evergarden pill " + className
+                : "evergarden pill"
+            }
+            {...props}
+        >
+            {children}
+        </span>
+    )
+}
+
+function Button(
+    { className="", mod, isLoading, children, ...props }
+    : {
+        className: string,
+        mod: null | "cta" | "warning" | "destructive",
+        isLoading: boolean,
+        children: ComponentChildren,
+        props: any
+    }
+) {
+    className += " evergarden";
+    className += mod? ` mod-${mod}` : "";
+    className += isLoading? " mod-loading": "";
+    return (
+        <button
+            className={className}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
+function ClickableIcon({
+    className, 
+    icon,
+    onClick,
+    isActive,
+    disabled,
+    label,
+    ...props
+}) {
+    return (
+        <div
+            className={
+                "clickable-icon " +
+                className +
+                (isActive? " is-active" : "")
+            }
+            aria-disabled={disabled}
+            aria-label={label}
+            onClick={onClick}
+            {...props}
+        >
+            <dc.Icon icon={icon} />
+        </div>
+    )
+}
+
+return {
+    InternalLink,
+    SelfHandledInternalLink,
+    ExternalLink,
+    Pill, 
+    Button,
+    ClickableIcon,
+}
